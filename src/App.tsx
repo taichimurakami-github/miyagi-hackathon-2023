aacac9688eabad1187e3338e34f00ca238acd2e0
taichi.murakami <tchi0017@gmail.com>
2023-03-03 14:57:58 +0900

    Add sdk and hosting examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import QREncoder from "./components/QREncoder";
+import { createSDKInstance } from "@pocketsign/in-app-service-sdk";
+import sdkSecrets from "../sdk-secrets.json";
+import PocketsignSDKExample from "./components/PocketsignSDKExample";
+sdkSecrets;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [sdkIntanceExists, setSdkInstanceExists] = useState(false);
+  const sdkInstance = useRef<any>(null);
+  console.log(sdkSecrets.config.pocketsign_service_id);
+
+  useEffect(() => {
+    (async () => {
+      const sdk = await createSDKInstance({
+        serviceId: sdkSecrets.config.pocketsign_service_id,
+      });
+
+      sdkInstance.current = sdk;
+      setSdkInstanceExists(true);
+    })();
+  }, []);
 
   return (
     <div className="App">
-      <QREncoder onLoaded={(v) => console.log(v)} />
+      <PocketsignSDKExample sdkInstance={sdkInstance.current} />
+      {/* <QREncoder onLoaded={(v) => console.log(v)} /> */}
     </div>
   );
 }
