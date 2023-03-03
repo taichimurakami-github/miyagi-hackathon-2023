a4cb02d7126d17d4d4e3e477099adb1e29ae611a
taichi.murakami <tchi0017@gmail.com>
2023-03-03 22:02:54 +0900

    Remove imports

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { TwinButtonsContainer } from "../components/ui/TwinButtonsContainer";
 
 export default function Index() {
   return (
